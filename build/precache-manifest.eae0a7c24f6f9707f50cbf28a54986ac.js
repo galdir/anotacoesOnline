404f09e78faee604a1df2cc3ac20042fbe933c32
Galdir Reges <galdir@gmail.com>
2020-10-16 11:12:17 -0300

    ini

---
+++ build/precache-manifest.eae0a7c24f6f9707f50cbf28a54986ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8208bad88f049c7125f7deaefb530d57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acedb33e1a62fa0033c6",
+    "url": "/static/css/main.2a42dfa7.chunk.css"
+  },
+  {
+    "revision": "3d89fbc86edb9d7f0288",
+    "url": "/static/js/2.46642ead.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.46642ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acedb33e1a62fa0033c6",
+    "url": "/static/js/main.da1397ec.chunk.js"
+  },
+  {
+    "revision": "d94f108e62f36120c537",
+    "url": "/static/js/runtime-main.19a164db.js"
+  }
+]);
